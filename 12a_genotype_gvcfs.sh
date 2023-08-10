c9a90e08f7862a3bdd08f1304bd63af087ec1fa8
lupoveda <75391863+lupoveda@users.noreply.github.com>
2023-08-10 15:30:49 +0200

    Update 12a_genotype_gvcfs.sh

--- 12a_genotype_gvcfs.sh
+++ 12a_genotype_gvcfs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-cd ~/Documents/TFM
+cd ~/Documents/TFM/OC_TFM
 
 gatk GenotypeGVCFs \
---reference data/reference/hg19.fasta \
+--reference ~/Documents/TFM/data/reference/hg19.fasta \
 --variant gendb://results/genomicsdb_germline \
 --intervals data/target_regions/intervals.list \
 --output results/variants_germline/germline_OCcohort.vcf
