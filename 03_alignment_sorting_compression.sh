ad0ad279193200f14ae3e2d1e686203de13513b8
lupoveda <75391863+lupoveda@users.noreply.github.com>
2023-08-10 15:08:57 +0200

    Update 03_alignment_sorting_compression.sh
    adjusted directories and name

--- 03_alignment_sorting_compression.sh
+++ 03_alignment_sorting_compression.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-cd ~/Documents/TFM
+cd ~/Documents/TFM/OC_TFM
 
-mkdir alignment_all
+mkdir alignment
 
 for SAMPLE in OC1_L001 OC1_L002 OC1_L003 OC1_L004 OC2_L001 OC2_L002 OC2_L003 OC2_L004 \
 OC3_L001 OC3_L002 OC3_L003 OC3_L004 OC4_L001 OC4_L002 OC4_L003 OC4_L004 \
