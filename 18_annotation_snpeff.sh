ab6c7da273c7bbde4caab3a5c15b0cebf13dc731
lupoveda <75391863+lupoveda@users.noreply.github.com>
2023-08-13 18:17:22 +0200

    Update 18_annotation_snpeff.sh
    Explanations to the code added

--- 18_annotation_snpeff.sh
+++ 18_annotation_snpeff.sh
@@ -2,10 +2,10 @@
 
 
 cd ~/Documents/TFM/OC_TFM/results/variants_germline
-mkdir annotation
+mkdir annotation_snpeff #created a new directory to store the annotation from snpeff
 
 snpEff -Xmx4g \
 -v \
-hg19 \
-germline_OCcohort.filtered.vcf \
-> annotation/germline_OCcohort.filtered.snpeff.vcf
+hg19 \ #specified the reference version
+germline_OCcohort.filtered.vcf \ #specified the input gvcf file
+> annotation_snpeff/germline_OCcohort.filtered.snpeff.vcf #the annotated gvcf file with snpeff -  ANN corresponds to the annotation information
