7b9e61514a52fc7a2dbd38f0442f309b7041dac1
lupoveda <75391863+lupoveda@users.noreply.github.com>
2023-08-13 18:29:08 +0200

    Update 16_filter_INDELs.sh
    explanations added

--- 16_filter_INDELs.sh
+++ 16_filter_INDELs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cd ~/Documents/TFM/OC_TFM/results/variants_germline
+#Filtered with parameters taken from the recommendations on the best practices from the GATK suite
 
 gatk VariantFiltration \
 --variant germline_OCcohort.INDEL.vcf \
@@ -8,4 +9,4 @@ gatk VariantFiltration \
 --filter-expression "QUAL < 30.0"               --filter-name "QUAL30" \
 --filter-expression "FS > 200.0"                --filter-name "FS200" \
 --filter-expression "ReadPosRankSum < -20.0"    --filter-name "ReadPosRankSum-20" \
---output germline_OCcohort.INDEL.filtered.vcf
+--output germline_OCcohort.INDEL.filtered.vcf #gvcf file containing the filtered called INDELs
