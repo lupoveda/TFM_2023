9364e7505996104557b17c022a5db3e994123af7
lupoveda <75391863+lupoveda@users.noreply.github.com>
2023-08-13 18:00:23 +0200

    Update 21_Functotator_functional_annotator.sh
    explanations added

--- 21_Functotator_functional_annotator.sh
+++ 21_Functotator_functional_annotator.sh
@@ -1,5 +1,5 @@
 cd ~/Documents/TFM/OC_TFM
-mkdir results/variants_somatic/annotation_funcotator
+mkdir results/variants_somatic/annotation_funcotator #We create a new directory to store the results of the annotation with Functotator
 
 for SAMPLE in OC1_L001 OC1_L002 OC1_L003 OC1_L004 OC2_L001 OC2_L002 OC2_L003 OC2_L004 \
 OC3_L001 OC3_L002 OC3_L003 OC3_L004 OC4_L001 OC4_L002 OC4_L003 OC4_L004 \
@@ -11,12 +11,12 @@ OC13_L001 OC13_L002
 do
 
    gatk Funcotator \
-     --variant ~/Documents/TFM/OC_TFM/results/variants_somatic/"SAMPLE".filtered_pon.vcf \
-     --reference ~/Documents/TFM/data/reference/hg19.fasta \
-     --ref-version hg19 \
-     --data-sources-path ~/Documents/TFM/OC_TFM/test_somatic/funcotator_dataSources.v1.7.20200521s \
-     --output ~/Documents/TFM/OC_TFM/results/variants_somatic/annotation_funcotator/"SAMPLE".funcotated.vcf \
-     --output-file-format VCF
+     --variant ~/Documents/TFM/OC_TFM/results/variants_somatic/"SAMPLE".filtered_pon.vcf \ #Input is the filtered variants called with Mutect2 generated with the parameters defined in script 20_somatic_calling_Mutect2.sh
+     --reference ~/Documents/TFM/data/reference/hg19.fasta \ #Required to specify the reference
+     --ref-version hg19 \ #and the version of the human reference used
+     --data-sources-path ~/Documents/TFM/OC_TFM/test_somatic/funcotator_dataSources.v1.7.20200521s \ #Previous to this step, we downloaded the recommended dataSources for funcotator as specified in the Best practice guidelines
+     --output ~/Documents/TFM/OC_TFM/results/variants_somatic/annotation_funcotator/"SAMPLE".funcotated.vcf \ #Specified the output file name 
+     --output-file-format VCF #Specified the file format, can be VCF or MAF
 
 done
 
