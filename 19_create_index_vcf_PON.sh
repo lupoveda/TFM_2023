5c2a93adce0ed8034b17ed2cfb7eb09dc35c8464
lupoveda <75391863+lupoveda@users.noreply.github.com>
2023-08-13 18:14:14 +0200

    Update 19_create_index_vcf_PON.sh
    Updated code, added the PON code and added explanations

--- 19_create_index_vcf_PON.sh
+++ 19_create_index_vcf_PON.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-cd ~/Documents/TFM/OC_TFM/test_somatic 
+cd ~/Documents/TFM/OC_TFM/test_somatic #Before conducting this step, I renamed, using BCFtools, the chromosome names of the downloaded vcf files so that it matches the chromosome names from the reference, hence the prefix renamed
+#The vcf files used to create the PON, are the ones recommemnded in the Best practices of the GATK suite
 
-gatk IndexFeatureFile -I renamed_somatic-af-only-gnomad.raw.sites.vcf.gz
+gatk IndexFeatureFile -I renamed_somatic-af-only-gnomad.raw.sites.vcf.gz #Indexing of the vcf file
 
-gatk IndexFeatureFile -I renamed_somatic-b37-Mutect2-exome-panel.vcf
+gatk IndexFeatureFile -I renamed_somatic-b37-Mutect2-exome-panel.vcf #Indexing of the vcf file
 
+gatk CreateSomaticPanelOfNormals \
+-R ~Documents/TFM/data/reference/hg19.fasta \ #Reference used for this analysis
+--germline-resource renamed_somatic-af-only-gnomad.raw.sites.vcf.gz \ #vcf containing known germline variation sites in the population, from the GnomAd database
+-V renamed_somatic-b37-Mutect2-exome-panel.vcf \ #vcf containing known variation sites from the exome panels of dbSNP
+-O pon.vcf.gz #output file name given to this panel of normals
