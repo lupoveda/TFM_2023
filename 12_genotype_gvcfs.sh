88d6fd67b799be45d9961bb54387171e2dd014c9
lupoveda <75391863+lupoveda@users.noreply.github.com>
2023-08-14 08:18:00 +0200

    Update 12_genotype_gvcfs.sh
    Information added on the parameters used

--- 12_genotype_gvcfs.sh
+++ 12_genotype_gvcfs.sh
@@ -2,8 +2,9 @@
 
 cd ~/Documents/TFM/OC_TFM
 
-gatk GenotypeGVCFs \
---reference ~/Documents/TFM/data/reference/hg19.fasta \
---variant gendb://results/genomicsdb_germline \
---intervals data/target_regions/intervals.list \
---output results/variants_germline/germline_OCcohort.vcf
+gatk GenotypeGVCFs \ #Perform joint genotyping on one or more samples pre-called with HaplotypeCaller. Performs joint genotyping on the GenomicsDB workspace created with GenomicsDBImport (script 11_create_genomicsdb_germline.sh)
+
+--reference ~/Documents/TFM/data/reference/hg19.fasta \ # Reference sequence file
+--variant gendb://results/genomicsdb_germline \ # A VCF file containing variants - the input samples possess genotype likelihoods produced by HaplotypeCaller with `-ERC GVCF` 
+--intervals data/target_regions/intervals.list \ # Genomic intervals over which to operate - BRCA2, tp53 and BRCA1
+--output results/variants_germline/germline_OCcohort.vcf # File to which variants should be written
