15f3aa55d31c68d8c633f84c5f057bd4dcc93864
lupoveda <75391863+lupoveda@users.noreply.github.com>
2023-08-14 08:02:07 +0200

    Update 17_merge_filtered_variants.sh
    Added information regarding the tool

--- 17_merge_filtered_variants.sh
+++ 17_merge_filtered_variants.sh
@@ -2,7 +2,7 @@
 
 cd ~/Documents/TFM/OC_TFM/results/variants_germline
 
-gatk MergeVcfs \
+gatk MergeVcfs \ #Combines multiple variant files into a single variant file.
 --INPUT germline_OCcohort.SNP.filtered.vcf \
 --INPUT germline_OCcohort.INDEL.filtered.vcf \
---OUTPUT germline_OCcohort.filtered.vcf
+--OUTPUT germline_OCcohort.filtered.vcf #The output VCF is already sorted according to the dictionary and by coordinate.
