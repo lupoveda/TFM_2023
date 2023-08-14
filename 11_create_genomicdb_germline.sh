7be25df09b1948c11e00bc2e2e0cf266811f45ac
lupoveda <75391863+lupoveda@users.noreply.github.com>
2023-08-14 08:29:15 +0200

    Update 11_create_genomicdb_germline.sh
    added information on the parameters used

--- 11_create_genomicdb_germline.sh
+++ 11_create_genomicdb_germline.sh
@@ -2,14 +2,15 @@
 
 cd ~/Documents/TFM/OC_TFM
 
-#Database was created
+#gatk GenomicsDBImport allows to import VCFs to GenomicsDB before joint genotyping - and allows adding samples at a later stage to the datastore and avoid having to remap all the samples everytime a new sample is sequenced 
+#Database is created first using one sample
 
 gatk --java-options "-Xmx4g -Xms4g" GenomicsDBImport \
---variant results/variants_germline/OC1_L001.HC.g.vcf \
---intervals data/target_regions/intervals.list \
---genomicsdb-workspace-path results/genomicsdb_germline
+--variant results/variants_germline/OC1_L001.HC.g.vcf \ #adds a GVCFs produced by in HaplotypeCaller with the `-ERC GVCF`
+--intervals data/target_regions/intervals.list \ #  file containing the genomic intervals over which to operate: BRCA2, tp53 and BRCA1
+--genomicsdb-workspace-path results/genomicsdb_germline #name and location of the datastore
 
-#Database gets updated with the rest of the samples
+#Database gets updated with the rest of the samples - for this we use a loop to add the samples into the newly created datastore. All samples are in a gvcf file format and were called by the Haplotypecaller with the '-ERC GVCF'. 
 
 for SAMPLE in OC1_L002 OC1_L003 OC1_L004 OC2_L001 OC2_L002 OC2_L003 OC2_L004 \
 OC3_L001 OC3_L002 OC3_L003 OC3_L004 OC4_L001 OC4_L002 OC4_L003 OC4_L004 \
@@ -23,8 +24,10 @@ do
 cd ~/Documents/TFM
 
 gatk --java-options "-Xmx4g -Xms4g" GenomicsDBImport \
---variant results/variants_germline/"$SAMPLE".HC.g.vcf \
---intervals data/target_regions/intervals.list \
---genomicsdb-update-workspace-path results/genomicsdb_germline
+--variant results/variants_germline/"$SAMPLE".HC.g.vcf \ #adds the remaining GVCFs produced by in HaplotypeCaller with the `-ERC GVCF`
+--intervals data/target_regions/intervals.list \ #  file containing the genomic intervals over which to operate: BRCA2, tp53 and BRCA1
+--genomicsdb-update-workspace-path results/genomicsdb_germline #updating this previously created datastore
+
 done
 
+#More Information: https://gatk.broadinstitute.org/hc/en-us/articles/360036883491-GenomicsDBImport
