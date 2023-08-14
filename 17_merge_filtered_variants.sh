513e4ae133cfc6caf65edc39725e709a821b6711
lupoveda <75391863+lupoveda@users.noreply.github.com>
2023-08-14 08:03:46 +0200

    Update 17_merge_filtered_variants.sh
    added link website for more info

--- 17_merge_filtered_variants.sh
+++ 17_merge_filtered_variants.sh
@@ -6,3 +6,4 @@ gatk MergeVcfs \ #Combines multiple variant files into a single variant file.
 --INPUT germline_OCcohort.SNP.filtered.vcf \
 --INPUT germline_OCcohort.INDEL.filtered.vcf \
 --OUTPUT germline_OCcohort.filtered.vcf #The output VCF is already sorted according to the dictionary and by coordinate.
+#More info: https://gatk.broadinstitute.org/hc/en-us/articles/360037226612-MergeVcfs-Picard-
