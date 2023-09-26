1f5b4cbb7960e81b529295c183a9fd561ea98751
lupoveda <75391863+lupoveda@users.noreply.github.com>
2023-09-26 14:57:12 +0200

    Update 04_add_readgroups.sh_spanish

--- 04_add_readgroups.sh
+++ 04_add_readgroups.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-cd ~/Documents/TFM/OC_TFM/alignment
+cd ~/Documents/TFM/OC_TFM/alignment #Cambia el directorio actual al camino especificado
 
-cat sample_rg_fields.txt | while read SAMPLE LB PU ID
+cat sample_rg_fields.txt | while read SAMPLE LB PU ID #Este comando lee el contenido del archivo "sample_rg_fields.txt" línea por línea y divide cada línea en cuatro partes: SAMPLE (nombre de la muestra), LB (identificador de la biblioteca), PU (unidad de plataforma), e ID (identificador de grupo de lecturas).
 do
-  gatk AddOrReplaceReadGroups \
-  --INPUT "$SAMPLE".bam \
-  --OUTPUT "$SAMPLE".rg.bam \
-  --RGLB "$LB" \ #RGLB: library identifier
-  --RGPU "$PU" \ #RGPU: platform unit. [FLOWCELL].[LANE].[SAMPLE BARCODE]
-  --RGPL ILLUMINA \ #RGPL corresponds to the platform, and this would be ILLUMINA
-  --RGSM "$SAMPLE" \ #RGSM: the sample
-  --RGID "$ID" #RGID: read group id
+  gatk AddOrReplaceReadGroups \ #Este comando de GATK se utiliza para agregar o reemplazar información de grupos de lecturas en un archivo BAM.
+  --INPUT "$SAMPLE".bam \ #Especifica el archivo BAM de entrada que se procesará. El nombre del archivo BAM se toma de la variable SAMPLE.
+  --OUTPUT "$SAMPLE".rg.bam \ #Especifica el nombre del archivo BAM de salida que contendrá la información de grupos de lecturas. El nuevo archivo se crea con el sufijo ".rg.bam"
+  --RGLB "$LB" \ #RGLB: library identifier -> Define el identificador de la biblioteca (library identifier) para el grupo de lecturas.
+  --RGPU "$PU" \ #RGPU: platform unit. [FLOWCELL].[LANE].[SAMPLE BARCODE] -> Define la unidad de plataforma (platform unit) para el grupo de lecturas. La estructura de PU es "[FLOWCELL].[LANE].[SAMPLE BARCODE]"
+  --RGPL ILLUMINA \ #RGPL corresponds to the platform, and this would be ILLUMINA -> Especifica la plataforma de secuenciación utilizada, que en este caso es "ILLUMINA"
+  --RGSM "$SAMPLE" \ #RGSM: the sample -> Define el nombre de la muestra para el grupo de lecturas.
+  --RGID "$ID" #RGID: read group id -> Define el identificador del grupo de lecturas.
 done 
+# Los archivos BAM de salida se crean con los nuevos grupos de lecturas y se guardan con el sufijo ".rg.bam"
