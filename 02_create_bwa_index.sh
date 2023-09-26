0ad7aa93379b0ed97435adaeafce0fdf0930ee21
lupoveda <75391863+lupoveda@users.noreply.github.com>
2023-09-26 14:47:36 +0200

    Update 02_create_bwa_index.sh_spanish

--- 02_create_bwa_index.sh
+++ 02_create_bwa_index.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cd ~/Documents/TFM/data/reference/
-bwa index hg19.fasta
+cd ~/Documents/TFM/data/reference/ #Cambia el directorio actual al camino especificado
+bwa index hg19.fasta #Ejecuta el comando bwa index para indexar la referencia llamada "hg19.fasta".
