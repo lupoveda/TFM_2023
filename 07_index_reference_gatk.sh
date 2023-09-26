c080f892468f9fc7358f631bd87aca1b21c193ee
lupoveda <75391863+lupoveda@users.noreply.github.com>
2023-09-26 16:36:04 +0200

    Update 07_index_reference_gatk.sh_spanish

--- 07_index_reference_gatk.sh
+++ 07_index_reference_gatk.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd ~/Documents/TFM/data 
+cd ~/Documents/TFM/data #Cambia el directorio actual al camino especificado
 
-samtools faidx reference/hg19.fasta
-gatk CreateSequenceDictionary --REFERENCE reference/hg19.fasta
+samtools faidx reference/hg19.fasta #Este comando utiliza la herramienta Samtools para crear un índice (.fai) para el archivo de referencia genómica "hg19.fasta".
+gatk CreateSequenceDictionary --REFERENCE reference/hg19.fasta #Este comando utiliza la herramienta GATK para crear un diccionario de secuencias para el archivo de referencia "hg19.fasta"
