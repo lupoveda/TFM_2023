3cfcca898e4e908847e2718b3603aef01cc5c462
lupoveda <75391863+lupoveda@users.noreply.github.com>
2023-10-01 14:18:17 +0200

    Update 17_merge_filtered_variants.sh

--- 17_merge_filtered_variants.sh
+++ 17_merge_filtered_variants.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-cd ~/Documents/TFM/OC_TFM/results/variants_germline
+cd ~/Documents/TFM/OC_TFM/results/variants_germline #Cambio y especificamos el directorio
 
-gatk MergeVcfs \ #Combines multiple variant files into a single variant file.
---INPUT germline_OCcohort.SNP.filtered.vcf \
---INPUT germline_OCcohort.INDEL.filtered.vcf \
---OUTPUT germline_OCcohort.filtered.vcf #The output VCF is already sorted according to the dictionary and by coordinate.
+gatk MergeVcfs \ #Esta herramienta combina multiples archivos VCF en un único archivo /Combines multiple variant files into a single variant file.
+--INPUT germline_OCcohort.SNP.filtered.vcf \ #se especifica el primer documento a combinar
+--INPUT germline_OCcohort.INDEL.filtered.vcf \ #se especifica el segundo documento a combinar
+--OUTPUT germline_OCcohort.filtered.vcf #Este es el archivo de salida que ya esta sorteado segun las coordinadas / The output VCF is already sorted according to the dictionary and by coordinate.
 #More info: https://gatk.broadinstitute.org/hc/en-us/articles/360037226612-MergeVcfs-Picard-
+
+#Este script permite combinar múltiples archivos de VCF o gVCF. 
